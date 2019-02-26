2a9229b578aa6fb8f0b151e7ff067a9bbb189b8a
Samxzeng <38000311+Samxzeng@users.noreply.github.com>
2019-02-26 17:57:37 +0800

    finish 5-1

--- 5-1.js
+++ 5-1.js
@@ -1,33 +1,39 @@
 
-// loop the string many time unitl nothing found
-//get ride of the Aa or aA things every loop
-//can use splice()
-
-console.log('a'==='A')
-console.log('a'=='A')
-console.log
 const fs = require('fs')
-
 const dataInput = fs.readFileSync('input5-1.txt', 'utf8')
-// console.log(dataInput)
 console.log('original length',dataInput.length)
+// console.log(dataInput)
 
+finalArray =[]
 function removePolarity(data){
     let dataarray = [...data]
+    // console.log(dataarray)
     const datacut = dataarray.map((element,index)=>{
-        console.log('element',element)
+        // console.log('element',element)
         if(element.toUpperCase()===element&&element.toLowerCase()===dataarray[index+1]){
-            dataarray[index+1]=""
-            element=""
+            dataarray[index+1]="remove"
+            element="remove"
+            return element
         } 
         else if(element.toLowerCase()===element&&element.toUpperCase()===dataarray[index+1]){
-            dataarray[index+1]=""
-            element=""
+            dataarray[index+1]="remove"
+            element="remove"
+            return element
             }
+        else{return element}
     })
-    return datacut.filter(x=>x!==0)
-}
-// removePolarity(dataInput)
 
-// console.log(dataInput.length)
+    const filterArray = datacut.filter(x=>x!=="remove")
+    if (filterArray.length!==dataarray.length){
+        removePolarity(filterArray) // multiple runs of function itself, to get result array no reduction
+    }
+    else{
+        console.log("yes")
+        console.log(filterArray.length)
+        finalArray.push(filterArray)
+    }
+}
+removePolarity(dataInput)
+console.log('result',finalArray[0].length)
+
 
