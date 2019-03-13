2b20abe976b401b778a0995e0188ba1c3c354074
samxzeng <38000311+Samxzeng@users.noreply.github.com>
2019-03-13 22:56:49 +0800

    finishe 6-1

--- 6-1.js
+++ 6-1.js
@@ -11,7 +11,7 @@ function cutString(str){
     return [left,right]
 }
 const arrayData = dataInput.map(element=>cutString(element))
-console.log(arrayData)
+// console.log(arrayData)
 
 xArray = arrayData.map(element=>element[0])
 yArray = arrayData.map(element=>element[1])
@@ -19,16 +19,12 @@ maxX=Math.max(...xArray)
 maxY=Math.max(...yArray)
 minX=Math.min(...xArray)
 minY=Math.min(...yArray)
-xRange=[minX,maxX]
-yRange=[minY,maxY]
-
-console.log(xRange,yRange)
 
 function findManhattanDistance([x1,y1],[x2,y2]){
    return Math.abs(x1-x2)+Math.abs(y1-y2)
 }
-console.log(findManhattanDistance([1,2],[4,5]))
-function calculateDistance (array){
+
+function calculateDistance (array,minX,maxX,minY,maxY){
     let counter ={}
     for (i=minX;i<maxX;i++){
         for(j=minY;j<maxY;j++){
@@ -43,8 +39,36 @@ function calculateDistance (array){
             //findout the smallest one
         }
     }
-    console.log(counter)
+    // console.log(counter)
+    return counter
 }
-calculateDistance(arrayData)
+const square = calculateDistance(arrayData,minX,maxX,minY,maxY)
+const squareLarge = calculateDistance(arrayData,minX-1,maxX+1,minY-1,maxY+1)
+
+function findoverlapBiggest(obj1,obj2){
+    let value1= Object.values(obj1)
+    let value2 = Object.values(obj2)
+    const finiteElement =value1.map((element,index)=>{
+        if (value2.includes(element)){
+            // console.log(index)
+            return [index,element]
+        } else{
+            return ['infinite',0]
+        }
+    })
+    console.log(finiteElement)
+    const maxResult = Math.max(...finiteElement.map(el=>el[1]))
+    
+    console.log(maxResult)
+    return maxResult
+}
+
+
+
+findoverlapBiggest(square,squareLarge)
+
+
+
+
 
 //try to enlarge mixX,minY,maxX, maxY to see which one will change
