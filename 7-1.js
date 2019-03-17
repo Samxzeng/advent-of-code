83959e950dce471b4b0171c85ba6058a456735f2
samxzeng <38000311+Samxzeng@users.noreply.github.com>
2019-03-17 18:54:33 +0800

    finish 7-1

--- 7-1.js
+++ 7-1.js
@@ -1,80 +1,58 @@
 const fs = require('fs')
 const dataInput = fs.readFileSync('input7-1.txt', 'utf8').split(/\r\n/gm)
 console.log(dataInput[0])
+console.log('length',dataInput.length)
 
-// //interating throuhg obj, using entries
-
-// const object1 = { foo: 'bar', baz: 42 };
-// console.log(Object.entries(object1)[1]);
-// // expected output: Array ["baz", 42]
-// const object2 = {  1: 'b', 2: 'c',0: 'a' };
-// console.log(Object.entries(object2)[2]);
-// // expected output: Array ["2", "c"]
-// const result = Object.entries(object2).sort((a, b) => a - b);//sort to compare number
-// console.log('result',result)
-// console.log(Object.entries(result)[1]);
-
-
-function sortToCondition(data){
+function findPendingPoint(data){
     const precon = data.map(el=>el[5])
     const active = data.map(el=>el[36])
-    let obj ={}
-    active.forEach((el,index)=>{
-        if (obj[el]===undefined){
-            obj[el]=[]
-            obj[el].push(precon[index])
-        } else{
-            obj[el].push(precon[index])
-        }
-    })
-    console.log(obj)
-    return obj
-    //get each d:[a,b,c], then a,b,c is the prerequisite for d
-}
-
-function findStartPoint(data){
-    const precon = data.map(el=>el[5])
-    const active = data.map(el=>el[36])
-    let startpoint=[]
+    let pendingPoint=[]
     precon.forEach(el=>{
         if(active.includes(el)===false){
-            startpoint.push(el)
+            pendingPoint.push(el)
         }
     })
-    console.log(startpoint)
-    return startpoint
+    return pendingPoint
 }
 
-function findActiveOrder(remainig,done, obj){
-    //array is remaing
-    //done is already activated
-    //obj to the source to check
+function getOneResult(array){
+    const selection = array.sort()
+    return selection[0]
 }
 
-//start with d:undefine
-function giveAlpha(array){
-
-    //return the alpha order of first one
+function removeResultsDone(data,doneArray){
+    const newData =data.filter(el=>{
+        return doneArray.includes(el[5])===false 
+    })
+    return newData
 }
 
-function checkActivation(){
-    //check with one is activated
+
+function findlastNum(data){
+    const precon = data.map(el=>el[5])
+    const active = data.map(el=>el[36])
+    const lastNum = active.filter(el=>{
+        return precon.includes(el)===false
+    })
+    return lastNum
 }
 
-function sortResult(data){
-    const preObj = sortToCondition(data)
-    // console.log(preObj)
-    const startPoint = findStartPoint(data)
-    // console.log(startPoint)
-    const activateNumber = checkActivation(preObj)
-    // const result=[]
-    // result.push(giveAlpha(activateNumber))
-    //push to result the checking
+
+function loopToResult(data){
+    let results=[]
+    for (i=0;i<26;i++){
+        let newData = removeResultsDone(data,results)
+        let pendingPoints = findPendingPoint(newData)
+        let theOne = getOneResult(pendingPoints)
+        results.push(theOne)
+    }
+    return results
 }
 
-sortResult(dataInput)
+const results = loopToResult(dataInput)
+const lastNum = [...new Set(findlastNum(dataInput))]//find the unique number of an array
 
+const str = results.filter(el=>el!==undefined).map(el=>el).reduce((a,c)=>a+c)+lastNum 
+// get full list in string
 
-// let obj={}, then find the start point
-//then run the selection and push the result
-//run many time = length <26
+console.log(str)
