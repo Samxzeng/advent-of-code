e4a063865d8018bebbd2b37fb02efcafc645c684
samxzeng <38000311+Samxzeng@users.noreply.github.com>
2019-03-18 07:27:14 +0800

    test

--- 7-2.js
+++ 7-2.js
@@ -9,23 +9,32 @@ console.log (starting)
 // this step is to try to find the longest path to achive from beginning to the end
 
 
-let finished =[]
-let inPrgross =[]
+// let finished =[]
+// let inPrgross =[]
+
+// let newData = info.removeResultsDone(input,finished)
+// console.log(newData)
+// let pendingPoints = info.findPendingPoint(newData)
+// // needtodo = pendingPoints - inprogress 
+
+// console.log(pendingPoints)
+
+// inPrgross.push([...new Set(pendingPoints)])
+// console.log(inPrgross)
+
+// let theOne = info.getOneResult(pendingPoints)
+// finished.push(theOne)
+// console.log(finished)
 
 
+let worker1=[]
+let worker2=[]
+let worker3=[]
+let worker4=[]
+let worker5=[]
 
-let newData = info.removeResultsDone(input,finished)
-console.log(newData)
-let pendingPoints = info.findPendingPoint(newData)
-// needtodo = pendingPoints - inprogress 
+let round1 = starting.sort()
+console.log(round1)
 
-console.log(pendingPoints)
-
-inPrgross.push([...new Set(pendingPoints)])
-console.log(inPrgross)
-
-let theOne = info.getOneResult(pendingPoints)
-finished.push(theOne)
-console.log(finished)
-
+//after one finished, considered round2
 
