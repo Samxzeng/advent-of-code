53c2b020decd936eceb85fdbd0ca13e76a5fc389
samxzeng <38000311+Samxzeng@users.noreply.github.com>
2019-03-17 11:29:38 +0800

    maybe not the right order

--- 7-1.js
+++ 7-1.js
@@ -1,37 +1,59 @@
 const fs = require('fs')
 const dataInput = fs.readFileSync('input7-1.txt', 'utf8').split(/\r\n/gm)
-// console.log(dataInput)
+console.log(dataInput[0])
 
-let abc={}
-abc['test']=[]
-abc['test'].push('give')
-console.log(abc) // test how to define type in js obj
+// //interating throuhg obj, using entries
 
-//interating throuhg obj, using entries
-const object1 = { foo: 'bar', baz: 42 };
-console.log(Object.entries(object1)[1]);
-// expected output: Array ["baz", 42]
-
-const object2 = {  1: 'b', 2: 'c',0: 'a' };
-console.log(Object.entries(object2)[2]);
-// expected output: Array ["2", "c"]
-
-const result = Object.entries(object2).sort((a, b) => a - b);//sort to compare number
-console.log('result',result)
-console.log(Object.entries(result)[1]);
+// const object1 = { foo: 'bar', baz: 42 };
+// console.log(Object.entries(object1)[1]);
+// // expected output: Array ["baz", 42]
+// const object2 = {  1: 'b', 2: 'c',0: 'a' };
+// console.log(Object.entries(object2)[2]);
+// // expected output: Array ["2", "c"]
+// const result = Object.entries(object2).sort((a, b) => a - b);//sort to compare number
+// console.log('result',result)
+// console.log(Object.entries(result)[1]);
 
 
-function getToArray(){
-    //transfer to array
+function sortToCondition(data){
+    const precon = data.map(el=>el[5])
+    const active = data.map(el=>el[36])
+    let obj ={}
+    active.forEach((el,index)=>{
+        if (obj[el]===undefined){
+            obj[el]=[]
+            obj[el].push(precon[index])
+        } else{
+            obj[el].push(precon[index])
+        }
+    })
+    console.log(obj)
+    return obj
+    //get each d:[a,b,c], then a,b,c is the prerequisite for d
 }
 
-function sortToCondition(){
-    //use getToArray() to transfer
-    //get each d:[a,b,c], then a,b,c is the prerequisite for d
+function findStartPoint(data){
+    const precon = data.map(el=>el[5])
+    const active = data.map(el=>el[36])
+    let startpoint=[]
+    precon.forEach(el=>{
+        if(active.includes(el)===false){
+            startpoint.push(el)
+        }
+    })
+    console.log(startpoint)
+    return startpoint
+}
+
+function findActiveOrder(remainig,done, obj){
+    //array is remaing
+    //done is already activated
+    //obj to the source to check
 }
 
 //start with d:undefine
 function giveAlpha(array){
+
     //return the alpha order of first one
 }
 
@@ -40,12 +62,15 @@ function checkActivation(){
 }
 
 function sortResult(data){
-    const preObj = sortToConditon(data)
+    const preObj = sortToCondition(data)
+    // console.log(preObj)
+    const startPoint = findStartPoint(data)
+    // console.log(startPoint)
     const activateNumber = checkActivation(preObj)
-    const result=[]
-    result.push(giveAlpha(activateNumber))
+    // const result=[]
+    // result.push(giveAlpha(activateNumber))
     //push to result the checking
 }
 
-// sortResult(dataInput)
+sortResult(dataInput)
 
