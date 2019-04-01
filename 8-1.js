fe7f4ca8a6ad48bb471f4d77c19be8d4b0e8b597
Samxzeng <38000311+Samxzeng@users.noreply.github.com>
2019-04-01 21:55:29 +0800

    there is some prob with this question

--- 8-1.js
+++ 8-1.js
@@ -1,8 +1,9 @@
 const fs = require('fs')
 const dataInput = fs.readFileSync('input8-1.txt', 'utf8').split(' ')
+console.log(dataInput.length)
+console.log(dataInput)
 
 const input ={0:dataInput.map(el=>el*1)} 
-console.log('last', input[0][input[0].length-1])
 
 function getMetadata(array){
     const metalength = array[1]
@@ -10,9 +11,10 @@ function getMetadata(array){
     return addup 
     //this function only return addup
 }
+// const children = new Array(3);
+// console.log(children)
 
-
-const testObj ={"0":[1,2,3,4,5,1,2,3,4,5,1,2,3,4,5]} 
+const testObj ={"0":[1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,1,2,3,4,5]} 
 const testarr =[1,2,3,4,5,1,2,3,4,5,1,2,3,4,5]
 
 
@@ -24,7 +26,7 @@ function findnewOject (object){
         const leftover = object[num].slice(2,object[num].length-object[num][1])
         // console.log(leftover)
         for (i=0;i<subnodes;i++){
-         newOject[num+'-'+i]=leftover.slice(Math.floor(leftover.length / subnodes *i),Math.floor(leftover.length / subnodes *(i+1)))
+         newOject[num+'-'+i]=leftover.slice(Math.ceil(leftover.length / subnodes *i), Math.ceil(leftover.length / subnodes *(i+1)))
         }
         
     } 
@@ -39,29 +41,29 @@ function addup(obj){
     return addup.reduce((a,c)=>{return a+c},0)
 }
 
-// console.log(addup(input))
 
-
-
-console.log(findnewOject(testObj),'1st')
-console.log(findnewOject(findnewOject(testObj)),'2nd')
+// console.log(findnewOject(testObj),'1st')
+// console.log(findnewOject(findnewOject(testObj)),'2nd')
+// console.log(findnewOject(findnewOject(findnewOject(testObj))),'3rd')
 
 // console.log(addup(testObj))
-// console.log(addup(findnewOject(input)))
-// console.log(addup(findnewOject(findnewOject(input))))
+// console.log(addup(findnewOject(testObj)))
+// console.log(addup(findnewOject(findnewOject(testObj))))
 
-var obj = {0:dataInput.map(el=>el*1)} 
-// var obj = testObj
-
-var sum = 0
-for (j=0;j<4;j++){
-    console.log('run'+j)
-    sum += addup(obj)
-    obj = findnewOject(obj)
-    // console.log(obj)
-    console.log('sum',sum)
-
+function getresult(){
+    var obj = {0:dataInput.map(el=>el*1)} 
+    var sum = 0
+    for (j=0;j<20;j++){
+        console.log('run'+j)
+        sum += addup(obj)
+        obj = findnewOject(obj)
+        // for (let num in obj){
+        //     console.log(obj[num].length)
+        // }
+        console.log('sum',sum)
+    
+    }
 }
-console.log(obj)
-
+getresult()
+console.log('input length', input[0].length)
 console.log(dataInput.map(el=>el*1).reduce((a,c)=>a+c))
