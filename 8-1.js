d1e2f6dd3c4bc4fa7db48781ac5e0b25d4612380
Samxzeng <38000311+Samxzeng@users.noreply.github.com>
2019-04-29 09:02:49 +0800

    start using generator and recur

--- 8-1.js
+++ 8-1.js
@@ -1,80 +1,10 @@
 const fs = require('fs')
-const dataInput = fs.readFileSync('input8-1.txt', 'utf8').split(' ')
-// console.log(dataInput.length)
-// console.log(dataInput)
+const dataInput = fs.readFileSync('input8-1.txt', 'utf8').split(" ").map(Number)
 
-const input ={0:dataInput.map(el=>el*1)} 
-
-function getMetadata(array){
-    const metalength = array[1]
-    const addup = array.slice(array.length-metalength,array.length).reduce((a,c)=>{return a+c},0)
-    return addup 
-    //this function only return addup
+function* getYield (){
+    yield* dataInput
 }
-
-const testObj ={"0":[2, 3, 0, 3, 10, 11, 12, 1, 1, 0, 1, 99, 2, 1, 1, 2]} 
-
-function findnewOject (object){
-    let newOject = {}
-    for (let num in object){
-        // console.log(object[num].length)
-        const subnodes = object[num][0]
-        const leftover = object[num].slice(2,object[num].length-object[num][1])
-        // console.log(leftover)
-        for (i=0;i<subnodes;i++){
-            let remainnumber=leftover.length % subnodes
-            if(i<subnodes-remainnumber){
-         newOject[num+'-'+i]=leftover.slice(Math.floor(leftover.length / subnodes) *i, Math.floor(leftover.length / subnodes) *(i+1))
-            }
-            else{
-         newOject[num+'-'+i]=leftover.slice(Math.floor(leftover.length / subnodes) *i, Math.floor(leftover.length / subnodes) *(i+1)+1)
-            }
-            // if(i<subnodes-1){
-            //     newOject[num+'-'+i]=leftover.slice(Math.floor(leftover.length / subnodes) *i, Math.floor(leftover.length / subnodes) *(i+1))
-            //        }
-            //        else{
-            //     newOject[num+'-'+i]=leftover.slice(Math.floor(leftover.length / subnodes) *i, Math.floor(leftover.length / subnodes) *(i+1)+remainnumber)
-            //        }
-        }
-        
-    } 
-    return newOject
-}
-
-function addup(obj){
-    let addup=[]
-    for (let num in obj){
-        addup.push(getMetadata(obj[num]))
-    }
-    return addup.reduce((a,c)=>{return a+c},0)
-}
+let input = getYield()
+// to get the datainput as generator, to use further the next() function in generator and recurring
 
 
-// console.log(findnewOject(testObj),'1st')
-// console.log(findnewOject(findnewOject(testObj)),'2nd')
-// console.log(findnewOject(findnewOject(findnewOject(testObj))),'3rd')
-
-// console.log(addup(testObj))
-// console.log(addup(findnewOject(testObj)))
-// console.log(addup(findnewOject(findnewOject(testObj))))
-
-function getresult(){
-    let obj = {0:dataInput.map(el=>el*1)} 
-    // let obj = testObj;
-    let sumNum =0 
-    for (j=0;j<21;j++){
-        console.log('run'+j)
-        sumNum = addup(obj)+sumNum
-        obj = findnewOject(obj)
-        // console.log(obj)
-        console.log(sumNum)
-        for (let n in obj){
-            // console.log(obj[n].length)
-        }
-    }
-}
-getresult()
-console.log('input length', input[0].length)// the length of the number 
-console.log(dataInput.map(el=>el*1).reduce((a,c)=>a+c))
-
-//problem:the data is not divided into integer
